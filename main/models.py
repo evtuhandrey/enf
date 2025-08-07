727f101564b1bb2987df1d31081fa6b21add7986
andrusha <evtuhandrey@mail.ru>
2025-08-07 18:37:34 +0300

    views/urls/admin

--- main/models.py
+++ main/models.py
@@ -54,4 +54,4 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE,
                                 related_name='images')
-    image = models.ImageField(upload_to='products/extra/')
+    image = models.ImageField(upload_to = 'products/extra/')
