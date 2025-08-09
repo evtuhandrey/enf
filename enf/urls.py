54eb4139a131aa8e594e717e33864be53af2d113
andrusha <evtuhandrey@mail.ru>
2025-08-09 20:21:34 +0300

    отредактировал шаблоны base и product_detail и исправил ошибки+код

--- enf/urls.py
+++ enf/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace = 'cart')),
-    path('',include('main.urls', namespace = 'main')),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('', include('main.urls', namespace='main')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
-                          document_root = settings.MEDIA_ROOT)
+                          document_root=settings.MEDIA_ROOT)
